81bfee86f2f78ad2f16ed3699d61f87088ef756f
AKK-soft-dev <aungkokosoftdev@gmail.com>
2023-08-15 10:23:45 +0630

    added popular data

--- components/home/popular/StreamPopular.tsx
+++ components/home/popular/StreamPopular.tsx
@@ -1,5 +1,6 @@
 import fetchData from "@/config/fetch";
 import PopularDataDisplay from "./PopularDataDisplay";
+import formatDate from "@/utils/format-date";
 
 function wait(duration: number) {
   return new Promise((resolve) => {
@@ -8,9 +9,14 @@ function wait(duration: number) {
 }
 
 export default async function StreamPopular() {
-  const res = await fetch("http://localhost:3000/api/popular/digital", {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&release_date.lte=${formatDate(
+      new Date()
+    )}&sort_by=popularity.desc&with_release_type=4`,
+    {
+      cache: "no-store",
+    }
+  );
 
   await wait(1000);
   const popularData = await res.json();
