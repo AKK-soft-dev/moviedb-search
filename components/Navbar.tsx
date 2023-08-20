8d9f6920d218f8ef61e0235e89980afedf1c749a
AKK-soft-dev <aungkokosoftdev@gmail.com>
2023-08-21 00:08:14 +0630

    added drawer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,12 +6,21 @@ import {
   Toolbar,
   Typography,
   Container,
+  Drawer,
   styled,
   TypographyProps,
+  List,
+  ListItemButton,
+  ListItemIcon,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import LiveTVIcon from "@mui/icons-material/LiveTv";
+import MovieIcon from "@mui/icons-material/LocalMovies";
+import PeopleIcon from "@mui/icons-material/Group";
 import Link from "next/link";
 import SearchIcon from "@mui/icons-material/Search";
 import Image from "next/image";
+import { useState } from "react";
 
 const StyledTypography = styled(Typography)<TypographyProps & { href: string }>(
   ({ theme }) => ({
@@ -27,25 +36,102 @@ const StyledTypography = styled(Typography)<TypographyProps & { href: string }>(
 );
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  const toggleDrawer = () => {
+    setOpen((prev) => !prev);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <AppBar position="sticky" elevation={1}>
       <Container>
         <Toolbar component="nav" sx={{ columnGap: 2 }} disableGutters>
-          <Link href="/">
-            <Image src="/icon.png" alt="App logo" width={32} height={32} />
-          </Link>
+          <IconButton
+            onClick={toggleDrawer}
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ display: { xs: "block", md: "none" }, flex: "0 0 auto" }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              elevation: 0,
+            }}
+            sx={{
+              "& .MuiPaper-root": {
+                minWidth: 120,
+              },
+            }}
+          >
+            <Toolbar />
+            <List>
+              <ListItemButton
+                LinkComponent={Link}
+                href="#"
+                divider
+                sx={{ py: 2 }}
+              >
+                <ListItemIcon sx={{ minWidth: 0, mr: 1 }}>
+                  <MovieIcon />
+                </ListItemIcon>
+                Movies
+              </ListItemButton>
+              <ListItemButton
+                LinkComponent={Link}
+                href="#"
+                divider
+                sx={{ py: 2 }}
+              >
+                <ListItemIcon sx={{ minWidth: 0, mr: 1 }}>
+                  <LiveTVIcon />
+                </ListItemIcon>
+                TV Shows
+              </ListItemButton>
+              <ListItemButton
+                LinkComponent={Link}
+                href="#"
+                divider
+                sx={{ py: 2 }}
+              >
+                <ListItemIcon sx={{ minWidth: 0, mr: 1 }}>
+                  <PeopleIcon />
+                </ListItemIcon>
+                People
+              </ListItemButton>
+            </List>
+          </Drawer>
 
-          <StyledTypography component={Link} variant="body1" href="#">
-            Movies
-          </StyledTypography>
-          <StyledTypography component={Link} variant="body1" href="#">
-            TV Shows
-          </StyledTypography>
-          <StyledTypography component={Link} variant="body1" href="#">
-            People
-          </StyledTypography>
+          <Box
+            sx={{
+              flex: "1 1 0",
+              display: "flex",
+              alignItems: "center",
+              columnGap: 2,
+            }}
+          >
+            <Box component={Link} href="/" sx={{ mx: { xs: "auto", md: 0 } }}>
+              <Image src="/icon.png" alt="App logo" width={32} height={32} />
+            </Box>
 
-          <Box sx={{ ml: "auto" }}>
+            <Box sx={{ display: { xs: "none", md: "flex" }, columnGap: 2 }}>
+              <StyledTypography component={Link} variant="body1" href="#">
+                Movies
+              </StyledTypography>
+              <StyledTypography component={Link} variant="body1" href="#">
+                TV Shows
+              </StyledTypography>
+              <StyledTypography component={Link} variant="body1" href="#">
+                People
+              </StyledTypography>
+            </Box>
+          </Box>
+
+          <Box sx={{ flex: "0 0 auto" }}>
             <IconButton>
               <SearchIcon />
             </IconButton>
