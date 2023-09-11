6420ed39ea2e56401e49cb59ff587497cf4f3c14
AKK-soft-dev <aungkokosoftdev@gmail.com>
2023-09-11 15:32:28 +0630

    refactor css file name generated by sass

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import ThemeRegistry from "@/context/ThemeRegistry";
 import "./globals.css";
 import "animate.css";
-import "../style/bs-grid.css";
+import "../style/bs-grid-and-wrapper.css";
 import "../node_modules/swiper/swiper-bundle.min.css";
 import type { Metadata } from "next";
 import { Box } from "@mui/material";
